7a889892a765b1fc743fdf9288151b85721cd6b0
Robbie <robbiealixsantos@gmail.com>
2019-11-22 11:28:35 -0800

    Add RSVP resources

---
+++ Controllers/RSVPController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace robbie_and_weinye_2020.Controllers
+{
+    public class RSVPController
+    {
+        private static readonly string[] Comments = new[]
+        {
+            "Thank you for inviting us!",
+            "You two are a beautiful couple",
+            "Congratulations!",
+            "Looking forward to it!",
+            "Great choice in venue!",
+            "I'm so happy for you two!",
+            "Woohoo! I'm so excited!!"
+        };
+
+        private readonly ILogger<RSVPController> _logger;
+
+        public RSVPController(ILogger<RSVPController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<RSVP> RSVPs()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new RSVP
+            {
+                CreatedDate = DateTime.Now.AddDays(index),
+                Name = "Test Testerson",
+                Email = "test@test.com",
+                RSVPCount = 1,
+                Comment = Comments[rng.Next(Comments.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ RSVP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace robbie_and_weinye_2020
+{
+    public class RSVP
+    {
+        public DateTime CreatedDate { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int RSVPCount { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
