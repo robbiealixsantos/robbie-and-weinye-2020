b2b4e2808c4ba3f7639f2a7f9940f99ad4e69743
Robbie <robbiealixsantos@gmail.com>
2019-11-22 11:59:51 -0800

    Update Home.js and associated resources, add Home.css

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="canvas">
+                <div id="upper-logo">
+                    <h1 class="logo" data-content-field="site-title">
+                        <a href="/">Robbie &amp; Weinye</a>
+                    </h1>
+                </div>
+
+                <div id="topNav">
+                    <nav class="main-nav" data-content-field="navigation">
+                        <ul>
+                            <li class="page-collection active-link">
+                                <a href="/">Home</a>
+                            </li>
+                            <li class="page-collection">
+                                <a href="/wedding">Wedding</a>
+                            </li>
+                            <li class="page-collection">
+                                <a href="/photos">Photos</a>
+                            </li>
+                            {/*<li class="page-collection">
+                                <a href="/registry">Registry</a>
+                            </li>*/}
+                            <li class="page-collection">
+                                <a href="/rsvps">RSVP</a>
+                            </li>
+                        </ul>
+                        <div class="page-divider"></div>
+                    </nav>
+                </div>
+
+                <div id="main-container">
+                    <img id="main-image" src={require('../images/robbie-weinye-champagne.jpg')} />
+                    <div class="page-divider"></div>
+                </div>
+
+                <div id="info-div">
+                    <h3 class="info-div-top-bottom">Come celebrate our wedding</h3>
+                    <div class="page-divider-info"></div>
+                    <div class="info-div-main">
+                        <h1><b>Robbie Santos</b></h1>
+                        <h2>-<em>&</em>-</h2>
+                        <h1><b>Weinye Chen</b></h1>
+                    </div>
+                    <div class="page-divider-info"></div>
+                    <h3 class="info-div-top-bottom">Saturday, August 1, 2020</h3>
+                </div>
+
+
+                <div class="footer-content-div">
+                    <div>Robbie and Weinye joyfully request the pleasure of your company as we speak our vows and join in marriage in the presence of family and friends. Photography by <a id="jane-link" target="_blank" href="https://www.hellojanelee.com/">Jane Lee</a>.</div>
+                </div>
+            </div>
+        );
+    }
 }
